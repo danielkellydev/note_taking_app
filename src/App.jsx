daf03207cb75eb5c084523dfe1455b1c7d6b39d5
danielkellydev <danielkellydev@gmail.com>
2023-05-18 09:30:06 +1000

    create route addresses

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route } from "react-router-dom"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <Routes>
+          <Route path= "/" element={<h1>TODO</h1>} />
+          <Route path= "/notes" element={<h1>TODO</h1>} />
+          <Route path= "/notes/:noteID" element={<h1>TODO</h1>} />
+          <Route path= "/notes/:noteID/edit" element={<h1>TODO</h1>} />
+          <Route path= "/notes/searchByWord/:word" element={<h1>TODO</h1>} />
+          <Route path= "/notes/sort/duedate" element={<h1>TODO</h1>} />
+          <Route path= "/notes/sort/createdate" element={<h1>TODO</h1>} />
+          <Route path= "/notes/filter/overdue" element={<h1>TODO</h1>} />
+          <Route path= "/notes/filter/done" element={<h1>TODO</h1>} />
+
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
   )
 }
 
