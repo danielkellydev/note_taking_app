17d7eb741b75a500648854c95c4aa31a494a98db
danielkellydev <danielkellydev@gmail.com>
2023-05-18 10:27:50 +1000

    NotesContext scaffolded

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import NotesProvider from './contexts/NotesContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-        <App />
+        <NotesProvider>
+            <App />
+        </NotesProvider>
     </BrowserRouter>
     
   </React.StrictMode>,
